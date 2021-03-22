570ec78802673da382c6bd849b922c28b0e77652
Home <Home@DESKTOP-9EIQKPF>
2021-03-22 19:45:12 +0530

    Commit succesfully !!!!

---
+++ bookstore/src/test/java/com/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+ class BookstoreApplicationTests {
+	
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
